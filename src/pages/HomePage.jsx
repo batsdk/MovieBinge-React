c8f0cfbba65a6bd5d779edc606310432bb1d20d7
shemil <shemilkaweesha@gmail.com>
2023-03-27 13:46:01 +0530

    Tailwind Init

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <>
+      <div className="text-3xl font-bold underline text-teal-800">
+        HomePagess
+      </div>
+    </>
+  );
 };
 
 export default HomePage;
